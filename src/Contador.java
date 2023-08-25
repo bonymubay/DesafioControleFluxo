1912b112317b15788688a4fee3d4fbd9d7e76571
Gerson Chelengo <122971587+bonymubay@users.noreply.github.com>
2023-08-25 04:08:24 -0800

    updat

--- src/Contador.java
+++ src/Contador.java
@@ -1,43 +1,39 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws ArithmeticException {
-
-        Scanner input= new Scanner(System.in);
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
 
-        int num1, num2;
+        DecimalFormat df = new DecimalFormat("#.#");
 
-        try {
 
-            System.out.println("Introduza 0 primeiro numero inteiro:");
-            num1 = input.nextInt();
 
-            System.out.println("Introduza o segundo numero inteiro:");
-            num2 = input.nextInt();
-            contar(num1, num2);
-            
-        }catch
-        (Exception t) {
 
-            System.out.println("Introduza apenas numeros inteiros!");
+        try{
+            System.out.println("Informe o seu sado atual");
+            double saldoAtual = scanner.nextDouble();
+            double numeroArredondado = Math.round(saldoAtual * 10.0) / 10.0;
 
+            System.out.println("Digite o valor a depositar: ");
+            double valorDeposito = scanner.nextDouble();
+            saldoAtual= saldoAtual+valorDeposito;
+
+            System.out.println("Quanto deseja retirar da sua conta: ");
+            double valorRetirada = scanner.nextDouble();
+            saldoAtual= saldoAtual-valorRetirada;
+
+
+            System.out.println("Seu sado: "+ df.format(saldoAtual));
+
+        }catch(Exception e){
+
+            System.out.println("Dados Ivalidos");
         }
 
+        //TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+
+        //TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
     }
-
-    public static void contar(int num1, int num2) throws ParametrosInvalidosException{
-
-        if(num1> num2){
-            throw new ParametrosInvalidosException("O Segundo numero deve ser maior que o Primeiro");
-
-
-        }
-        else {
-            for (int i = 0; i <=num2-num1; i++) {
-                System.out.println("i = " + (++i));
-            }
-        }
-        }
-
 }
